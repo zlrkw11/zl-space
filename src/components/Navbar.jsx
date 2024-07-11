9470490ad8784e4050fd584247bd13c8cfe8b31a
ZL <ray040406@gmail.com>
2024-07-11 22:18:56 +1200

    structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 const Navbar= () => {
-    return (  );
+    return (
+        <nav className="navbar">
+            <h1>ZL blog</h1>
+            <div className="links">
+                <a href='/'>Home</a>
+                <a href='/create'>New Blog</a>
+                
+            </div>
+        </nav>
+    );
 }
  
 export default ;
