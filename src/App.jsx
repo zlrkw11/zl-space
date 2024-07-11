c77a6dfeb4bf4f210dd32235a29f4a234036e679
ZL <ray040406@gmail.com>
2024-07-11 19:36:27 +1200

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ function App() {
   const title = "Welcome to the new blog";
   const likes = 50;
   const person = { name: "zl", age: 20 };
+  const link = "http://www.google.com";
   return (
     <div className="App">
       <div className="content">
@@ -12,6 +13,8 @@ function App() {
         <p>{"Hello World"}</p>
         <p>{[1, 2, 3, 4, 5]}</p>
         <p>{Math.random() * 10}</p>
+
+        <a href={link}></a>
       </div>
     </div>
   ); // div injected into index.html
--- src/template.md
+++ src/template.md
@@ -4,3 +4,15 @@
 
 React converts data to a **string** for displaying to the browser.
 excepts for Boolean & objects
+
+dynamic value can be stored in a variable:
+
+```
+const link = "http://www.google.com";
+```
+
+and used like:
+
+```
+<a href={link}></a>
+```
