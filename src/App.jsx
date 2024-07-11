58d7d19571a52830094231ea7ce4d0b4e2f583a5
ZL <ray040406@gmail.com>
2024-07-11 22:23:54 +1200

    home page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
         <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
