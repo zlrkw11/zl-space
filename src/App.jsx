637514a23a3de867601313303c678809465931bd
ZL <ray040406@gmail.com>
2024-07-11 22:15:41 +1200

    clear app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
 function App() {
-  // any JavaScript before return
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const person = { name: "zl", age: 20 };
-  const link = "http://www.google.com";
   return (
     <div className="App">
       <div className="content">
         <h1>{title}</h1>
-        <p>Liked {likes} times</p>
-        <p>{10}</p>
-        <p>{"Hello World"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random() * 10}</p>
-
-        <a href={link}></a>
       </div>
     </div>
-  ); // div injected into index.html
+  );
 }
 
 export default App;
---
+++ src/templateExample.jsx
@@ -0,0 +1,24 @@
+function App() {
+  // any JavaScript before return
+  const title = "Welcome to the new blog";
+  const likes = 50;
+  // cant use object
+  // const person = { name: "zl", age: 20 };
+  const link = "http://www.google.com";
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{10}</p>
+        <p>{"Hello World"}</p>
+        <p>{[1, 2, 3, 4, 5]}</p>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>Google site</a>
+      </div>
+    </div>
+  ); // div injected into index.html
+}
+
+export default App;
