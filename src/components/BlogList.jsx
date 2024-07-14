22c152bb78cd7c22fdd1861919f7948c6acf8c56
ZL <ray040406@gmail.com>
2024-07-14 16:46:13 +1200

    button text

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -10,7 +10,7 @@ const BlogList = ({ blogs, title, handleDelete }) => {
             <h2>{blog.title}</h2>
             <p>Written by {blog.author}</p>
           </div>
-          <button onClick={() => handleDelete(blog.id)}></button>
+          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
