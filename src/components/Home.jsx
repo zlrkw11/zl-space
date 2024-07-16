d5634ce9c29b94e8b3b8cffe6ffdc910ff9aacb5
ZL <ray040406@gmail.com>
2024-07-16 23:00:44 +1200

    loading

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
+  const [isPending, setIsPending] = useState(true);
   const [name, setName] = useState("rukawa");
 
   useEffect(() => {
@@ -20,6 +20,11 @@ const Home = () => {
 
   return (
     <div className="home">
+      {isPending && (
+        <div>
+          <p>Loading...</p>
+        </div>
+      )}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       <button onClick={() => setName("kaede")}>change name</button>
       <p>{name}</p>
