40f266a1e0982e7e2cdaf714d263ef990f5dc11e
ZL <ray040406@gmail.com>
2024-07-17 21:39:57 +1200

    not ok

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,6 +8,9 @@ const Home = () => {
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data for that resource");
+        }
         return res.json();
       })
       .then((data) => {
