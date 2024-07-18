13379d0b7b67014361f8121a1604a48fc055adf2
ZL <ray040406@gmail.com>
2024-07-19 00:00:59 +1200

    apply

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { data, isPending, error } = useFetch("http://localhost:8000/");
+  const { data, isPending, error } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
       {<p>{error}</p>}
@@ -12,7 +11,7 @@ const Home = () => {
           <p>Loading...</p>
         </div>
       )}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {data && <BlogList blogs={data} title="All Blogs" />}
     </div>
   );
 };
