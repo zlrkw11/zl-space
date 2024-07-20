15fdbf7738a2518e43b875cc3eaeed620875ac25
ZL <ray040406@gmail.com>
2024-07-20 22:30:28 +1200

    link

---
+++ notes/router/link.md
@@ -0,0 +1,7 @@
+# Link
+
+import
+
+```
+import {Link} from 'react-router-dom';
+```
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>ZL blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
           style={{
             color: "white",
             backgroundColor: "#f1356d",
@@ -13,7 +14,7 @@ const Navbar = () => {
           }}
         >
           New Blog
-        </a>
+        </Link>
       </div>
     </nav>
   );
