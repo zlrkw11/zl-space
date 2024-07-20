a2cb473ce0ceddf2499e9e051b1763c8478e1cf9
ZL <ray040406@gmail.com>
2024-07-20 22:41:27 +1200

    associate a c with the fetch

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -5,7 +5,9 @@ const useFetch = (url) => {
   const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
   useEffect(() => {
-    fetch(url)
+    const abortCont = new AbortController();
+
+    fetch(url, { signal: abortCont.signal })
       .then((res) => {
         if (!res.ok) {
           throw Error("could not fetch the data for that resource");
