5ada8d9ba42d6b12da2c8a55e7739802dda6db7e
ZL <ray040406@gmail.com>
2024-07-21 13:35:56 +1200

    frame of blog

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -10,7 +10,15 @@ const BlogDetails = () => {
   } = useFetch("https://localhost:8000/blogs/" + id);
   return (
     <div className="blog-details">
-      <h2>Blog Details - {id}</h2>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
     </div>
   );
 };
