4bc1b3f3e8197094d3a8c2465837df0e56df2186
ZL <ray040406@gmail.com>
2024-09-03 00:05:19 +1200

    body text

--- src/Create.jsx
+++ src/Create.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Create = () => {
   const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   return (
     <div className="create">
@@ -14,9 +15,14 @@ const Create = () => {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        <p>{title}</p>
 
         <label>Blog Body:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
 
         <label>Blog Author:</label>
         <select>
