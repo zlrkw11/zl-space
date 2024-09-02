a167a329bc30f1c9eb662c007d19c542755f27ec
ZL <ray040406@gmail.com>
2024-09-02 23:54:18 +1200

    re added

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,12 +1,14 @@
 const Create = () => {
   return (
     <div>
-      <h2>Add a new blog</h2>
+      <h2>Add Blog</h2>
       <form>
         <label>Blog Title:</label>
-        <inputt type="text" required />
+        <input type="text" required />
+
         <label>Blog Body:</label>
-        <textarea>required</textarea>
+        <textarea required></textarea>
+
         <label>Blog Author:</label>
         <select>
           <option value="ray">ray</option>
