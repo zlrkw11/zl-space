95e8be6a455101af95b2988d5d7fc0c5c48060a9
ZL <ray040406@gmail.com>
2024-09-02 23:44:00 +1200

    form component

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,5 +1,21 @@
 const Create = () => {
-  return <div>Add a new blog</div>;
+  return (
+    <div>
+      <h2>Add a new blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <inputt type="text" required />
+        <label>Blog Body:</label>
+        <textarea>required</textarea>
+        <label>Blog Author:</label>
+        <select>
+          <option value="ray">ray</option>
+          <option value="nate">nate</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 };
 
 export default Create;
